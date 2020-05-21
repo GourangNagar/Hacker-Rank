8eaab403b1104898930ddf40bf85273548b53b56
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-05-22 01:09:07 +0530

    Update code.py

--- 30-Days-of-Code/Day B-11/code.py
+++ 30-Days-of-Code/Day B-11/code.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
     
-    max=0
+    max= -100 #code only work properly for (negative default) because if we place 0 as default if fails for negetive inputs.
     for i in range(4):
         for j in range(4):
             sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
